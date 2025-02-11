77426a628659ae5e77ee92436fbbfa484daf2196
msilveus <michaelsilveus@gmail.com>
2025-02-11 13:43:34 -0800

    Initial source file checkin

---
+++ src/SimpleQueue.h
@@ -0,0 +1,28 @@
+// SimpleQueue.h
+#ifndef SIMPLE_QUEUE_H
+#define SIMPLE_QUEUE_H
+
+#include <Arduino.h>
+
+class SimpleQueue {
+public:
+    SimpleQueue(void *buffer, size_t element_size, size_t capacity);
+    void reset();
+    size_t count() const;
+    bool isEmpty() const;
+    bool isFull() const;
+    bool push(const void *element);
+    bool pushCyclic(const void *element);
+    bool pop(void *element);
+    void *peek(size_t index) const;
+
+private:
+    volatile int head;
+    volatile int tail;
+    volatile size_t size;
+    size_t capacity;
+    void *queue;
+    size_t element_size;
+};
+
+#endif /* SIMPLE_QUEUE_H */
